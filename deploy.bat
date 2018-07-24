55789f23daca2b89c73c3e3e2e066f7b6a43e73d
jeffy555 <jeffersonimmanuel5@gmail.com>
2018-07-24 15:28:11 +0530

    deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,2 @@
 echo "Deployment started"
-xcopy /Y /S /i "C:\Program Files (x86)\Jenkins\workspace\Pipeline_Project" "D:/Automation/"
+xcopy /Y /S /i "C:\Program Files (x86)\Jenkins\workspace\Pipeline_Project" "D:\Automation\Deployment"
